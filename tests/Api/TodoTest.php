81e52b7624e9f03e7ee4ce28c27e537d55156f6d
ilvsxk <ilvsxk@gmail.com>
2016-08-14 17:03:21 +0800

    Update TodoTest

--- tests/Api/TodoTest.php
+++ tests/Api/TodoTest.php
@@ -5,15 +5,22 @@ include_once 'BaseApiTestCase.php';
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Someline\Models\Foundation\Todo;
 
 class TodoTest extends BaseApiTestCase
 {
+    use DatabaseTransactions;
 
     /** @test */
     public function get_todos(){
+        $todos = factory(Todo::class, 3)->create();
 
         $this->get('todos');
-        $this->assertEquals('todos', $this->response->content());
+        $todos->map(function(Todo $todo){
+            $this->seeJson([
+                'title' => $todo->title
+            ]);
+        });
 
     }
 
