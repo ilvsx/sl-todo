7ebfa639f0dab5ce51a22e79b1498908050ff97d
ilvsxk <ilvsxk@gmail.com>
2016-08-14 21:46:33 +0800

    Update Todos.vue

--- resources/assets/js/vue/components/Todos.vue
+++ resources/assets/js/vue/components/Todos.vue
@@ -1,6 +1,11 @@
 <template>
 
-<h1>TODOS</h1>
+    <h1>TODOS</h1>
+    <input v-model="newTodo">
+    <button v-on:click="addTodo">+</button>
+    <ul v-for="todo in todos">
+        <li>{{todo.title}} <button v-on:click="delTodo(todo)">-</button> </li>
+    </ul>
 
 </template>
 
@@ -8,4 +13,43 @@
 </style>
 
 <script>
+    export default{
+        data(){
+            return {
+                todos: [
+                    {title: 'a'},
+                    {title: 'b'},
+                ],
+                newTodo: ''
+            }
+        },
+        http: {
+            root: '/api',
+            headers: {
+                Accept: 'application/x.someline.v1+json'
+            }
+        },
+        ready() {
+            this.get_todos();
+        },
+        methods: {
+            get_todos(){
+                this.todos = [
+                    {title: 'c'},
+                    {title: 'd'},
+                ]
+            },
+            addTodo(){
+                this.todos.push(
+                    {title: this.newTodo}
+                )
+            },
+            delTodo(todo){
+                this.todos.$remove(todo);
+            },
+            editTodo(todo, title){
+                todo.title = title;
+            }
+        }
+    }
 </script>
