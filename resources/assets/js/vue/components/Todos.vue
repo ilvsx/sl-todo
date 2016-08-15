c78f5503a74f32a4e22f9d163b73b9f353535edc
ilvsxk <ilvsxk@gmail.com>
2016-08-15 11:26:51 +0800

    Update Todos.vue

--- resources/assets/js/vue/components/Todos.vue
+++ resources/assets/js/vue/components/Todos.vue
@@ -3,7 +3,7 @@
     <h1>Todos</h1>
     <input v-model="newTodo">
     <button v-on:click="addTodo">+</button>
-    <ul v-for="todo in todos">
+    <ul v-for="todo in undoneTodos">
         <li>{{todo.title}}
             <button v-on:click="delTodo(todo)">Del</button>
             <button v-on:click="doneTodo(todo)">Done</button>
@@ -14,7 +14,9 @@
 
     <h1>Done Todos</h1>
     <ul v-for="todo in doneTodos">
-        <li>{{todo.title}}</li>
+        <li>{{todo.title}}
+            <button v-on:click="delTodo(todo)">Del</button>
+        </li>
     </ul>
 
 </template>
@@ -26,11 +28,12 @@
     export default{
         data(){
             return {
-                todos: [
+                undoneTodos: [
                     {id: 1, title: 'a'},
                     {id: 2, title: 'b'},
                 ],
                 doneTodos: [
+                    {id: 3, title: 'c'},
                 ],
                 newTodo: ''
             }
@@ -42,34 +45,44 @@
             }
         },
         ready() {
-            this.get_todos();
+            this.getUndoneTodos();
+            this.getDoneTodos();
         },
         methods: {
-            get_todos(){
-                var resource = this.$resource('todos', {});
+            getUndoneTodos(is_done){
+                var resource = this.$resource('todos' + '?search=is_done:0', {});
 
                 resource.get().then(function (response) {
-                    this.$set('todos', response.data.data);
+                    this.$set('undoneTodos', response.data.data);
+                });
+            },
+            getDoneTodos(){
+                var resource = this.$resource('todos' + '?search=is_done:1', {});
+
+                resource.get().then(function (response) {
+                    this.$set('doneTodos', response.data.data);
                 });
             },
             addTodo(){
                 var resource = this.$resource('todos', {});
 
                 resource.save({title: this.newTodo}).then(function (response) {
-                    this.todos.push(response.data.data);
+                    this.undoneTodos.push(response.data.data);
                 });
             },
             delTodo(todo){
                 var resource = this.$resource('todos{/id}', {});
 
                 resource.delete({id: todo.id}).then(function () {
-                    this.todos.$remove(todo);
+                    this.undoneTodos.$remove(todo);
+                    this.doneTodos.$remove(todo);
                 });
             },
             doneTodo(todo){
                 var resource = this.$resource('todos{/id}', {});
 
                 resource.update({id: todo.id}, {is_done: 1}).then(function () {
+                    this.undoneTodos.$remove(todo);
                     this.doneTodos.push(todo);
                 });
             }
